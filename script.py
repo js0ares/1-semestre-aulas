10b13650bd568811ea8338aa45686cb2172fca12
js0ares <joaosoares112006@gmail.com>
2025-02-27 21:20:03 -0300

    Add files via upload
    Aula 2.0

--- script.py
+++ script.py
@@ -1,9 +1,4 @@
-import math
-
-x = float(input("insira o valor do salario: "))
-z = float(input("insira a porcentagem: "))
-
-final = (x * z / 100) + x 
-novo_salario = x or z*(1 + porcentagem/100)
-
-print(f"o seu novo salario é: {novo_salario: .2f}")
+i = 1
+while i < 20:
+    print (i)
+    i += 1
