0ec72457c1f2e394d7a814605e69e33a5ba6e698
js0ares <joaosoares112006@gmail.com>
2025-03-18 21:11:21 -0300

    Add files via upload

---
+++ for i in 2.py
@@ -0,0 +1,5 @@
+soma = 0
+for i in "eu estudo no unasp":
+    if i == "u":
+      soma += 1
+print(soma)
---
+++ for i in 3.py
@@ -0,0 +1,3 @@
+# list comprenhension
+x = ([i for i in "eu estudo no unasp" if i != " "])
+print(len(x))
