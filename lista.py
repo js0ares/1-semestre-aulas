91b3efc349568934aedc8f53b70affe3846faba0
js0ares <joaosoares112006@gmail.com>
2025-04-01 19:39:22 -0300

    Add files via upload

--- lista.py
+++ lista.py
@@ -1,2 +1,7 @@
-x = [1, 2, 3,  True, "unasp"] 
-print(x[0:5])
+def media_lista(lista):
+    soma = 0
+    for i in lista:
+      soma += i
+    return soma/len(lista)
+
+print(media_lista({1, 2, 8, 9}))
